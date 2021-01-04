7bc952782ca4a436d32af76df84d7eb6085b399d
Snehal Mundhe <m.snehal106@gmail.com>
2021-01-03 19:48:24 -0500

    feat: changed css

--- ig-reels/src/App.js
+++ ig-reels/src/App.js
@@ -17,12 +17,12 @@ function App() {
     //BEM naming convention
     <div className="app">
       <div className="app__top">
-        <img 
+        {/* <img 
           className="app__logo"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
           alt=""
         />
-      
+       */}
         {/* <h1>Reels</h1> */}
       </div>
       <br/>
